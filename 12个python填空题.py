82c19187ffe8f3da2e1268179f8d5413691c6298
luis <121397414@qq.com>
2015-11-23 11:10:52 +0800

    增加装饰器模式

---
+++ 12个python填空题.py
@@ -0,0 +1,10 @@
+__author__ = 'LIUYuanYuan'
+# -*- coding:utf-8 -*-
+
+
+#>>> x, y = ???
+#>>> min(x, y) == min(y, x)
+#False
+
+x, y = {0}, {1}
+print(min(x, y ) == min(x, y))
---
+++ 装饰模式.py
@@ -0,0 +1,82 @@
+__author__ = 'LIUYuanYuan'
+# -*- coding:utf-8 -*-
+
+#模式特点：增加一个修饰类包裹原来的类，装饰过的对象可替代原始对象
+#原则：类应该对扩展开放，对修改关闭
+
+class Beverage(object):
+    description = 'Unknown Beverage'
+
+    def get_description(self):
+        return self.description
+
+    def cost(self):
+        pass
+
+class CondimentDecorator(Beverage):
+    def get_description(self):
+        pass
+
+class MilkyTea(Beverage):
+    def __init__(self):
+        self.description = "MilkyTea"
+    def cost(self):
+        return 1.99
+
+class FruitJuice(Beverage):
+    def __init__(self):
+        self.description = "FruitJuice"
+    def cost(self):
+        return 1.80
+
+class Coffee(Beverage):
+    def __init__(self):
+        self.description = "Coffee"
+    def cost(self):
+        return 2.00
+
+class pearl(CondimentDecorator):
+    def __init__(self, beverage):
+        self.beverage = beverage
+
+    def get_description(self):
+        return self.beverage.get_description()
+
+    def cost(self):
+        return 1.50 + self.beverage.cost()
+
+class Pudding(CondimentDecorator):
+    def __init__(self, beverage):
+        self.beverage = beverage
+    def get_description(self):
+        return self.beverage.get_description()
+    def cost(self):
+        return 1.60 + self.beverage.cost()
+
+class Milk(CondimentDecorator):
+    def __init__(self, beverage):
+        self.beverage = beverage
+    def get_description(self):
+        return self.beverage.get_description()
+    def cost(self):
+        return 2.10 + self.beverage.cost()
+
+if __name__ == '__main__':
+    b = FruitJuice()
+    print('%s = $%s' %(b.get_description(), b.cost()) )
+
+    b = MilkyTea()
+    b = pearl(b)
+    b = Pudding(b)
+    print('%s = $%s' %(b.get_description(), b.cost()) )
+
+    b = Coffee()
+    b = pearl(b)
+    b = Pudding(b)
+    print('%s = $%s' %(b.get_description(), b.cost()) )
+
+    b = Coffee()
+    b = pearl(b)
+    b = Milk(b)
+    print('%s = $%s' %(b.get_description(), b.cost()) )
+
